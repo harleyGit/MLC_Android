099cd4643971f9b1148d77f867ca49264cf6c820
GangHuang <harleysor@qq.com>
2024-05-18 15:09:29 +0800

    feat: 促使Android学习项目2.0版

---
+++ app/src/main/java/com/ganghuang/mlc2_android/FirstActivity.java
@@ -0,0 +1,18 @@
+package com.ganghuang.mlc2_android;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+    }
+}
